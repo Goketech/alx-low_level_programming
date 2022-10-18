17f8d9dac82fbab34f8d64a48a3535d3bf802553
Modupe Akanni <modupe775@gmail.com>
2022-10-18 09:07:00 -0700

    functions nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,14 +6,14 @@
 int main(void)
 {
 int count;
-unsigned long i , j, k;
+unsigned long i, j, k;
 unsigned long m, n, p, carry;
 count = 0;
 i = 0;
 j = 1;
 for (count = 1; count <= 91; count++)
 {
-k = i +j;
+k = i + j;
 i = j;
 j = k;
 printf("%lu, ", k);
